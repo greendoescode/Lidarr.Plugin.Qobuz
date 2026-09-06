821d4a77fe826eeb86e2eeef79bd8948975b1c94
leo <77177015+greendoescode@users.noreply.github.com>
2026-09-06 16:07:57 +0100

    Update Plugin.cs

--- src/Lidarr.Plugin.Qobuz/Plugin.cs
+++ src/Lidarr.Plugin.Qobuz/Plugin.cs
@@ -3,7 +3,7 @@ namespace NzbDrone.Core.Plugins
     public class QobuzPlugin : Plugin
     {
         public override string Name => "Qobuz";
-        public override string Owner => "TrevTV";
+        public override string Owner => "greendoescode";
         public override string GithubUrl => "https://github.com/greendoescode/Lidarr.Plugin.Qobuz";
     }
 }
